94a64303a4b3ad86fdb6b96c0f604c9cfd64f325
Aurélie PERRIN <aurelie.perrin9@gmail.com>
2014-11-13 18:58:21 +0100

    Ajout du module d'inscription (problème dans le mail de confirmation)

--- app/models/utilisateur.rb
+++ app/models/utilisateur.rb
@@ -3,4 +3,6 @@ class Utilisateur < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates_confirmation_of :mail, message: "Les mails doivent etre identiques"
+  validates_presence_of :nom, :prenom, :mail_confirmation, :password_confirmation
 end
