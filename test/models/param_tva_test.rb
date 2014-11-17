b137b76c1113fd93fd445fc1dbe3f0bb879c1df8
MangelMaxime <mangel.maxime@outlook.com>
2014-11-17 09:57:28 +0100

    Added test : BUG utilisateurs

--- test/models/param_tva_test.rb
+++ test/models/param_tva_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ParamTvaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "the truth" do
+    assert true
+  end
+  
+  test "ne_pas_enregister_une_TVA_sans_label" do
+    tva = ParamTVA.new
+    assert_not tva.save
+  end
+  
+  test "ne_pas_enregister_une_TVA_sans_valeur" do
+    tva = ParamTVA.new
+    assert_not tva.save
+  end
+  
 end
