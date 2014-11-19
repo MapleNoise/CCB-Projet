5721c9c4239a6b841b1a366f9a4306d330b159c4
MPur <dev.mpur@gmail.com>
2014-11-19 18:13:27 +0100

    implémentation de l'ajout du role utilisateur par défaut lors de la création. Les test ne sont pas concluant

--- app/models/utilisateur.rb
+++ app/models/utilisateur.rb
@@ -1,6 +1,9 @@
 class Utilisateur < ActiveRecord::Base
-  rolify :before_add => :before_add_method
+
+  rolify
   
+  after_create :assign_default_role!
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -8,14 +11,12 @@ class Utilisateur < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   validates_confirmation_of :email, message: "Les mails doivent etre identiques"
   validates_presence_of :nom, :prenom, :email_confirmation#, :password_confirmation
+ 
   
-  def before_add_method(role)
-    if not self.has_role? :admin or self.has_role? :coach
-      self.add_role :utilisateur
-    else 
-      self.add_role :utilisateur
-    end
-    
+  protected
+
+  def assign_default_role!
+    self.add_role :utilisateur
   end
-  
+
 end
