7a5a37be92344bb39752ef30e2a528354eccdfb9
ThomasAy <thomas@phonations.com>
2014-12-02 08:04:24 +0100

    TU produit

--- test/models/produit_test.rb
+++ test/models/produit_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class ProduitTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "Presence de ref" do
+		pt = Produit.create(nom: "name", prix: 0)
+		assert_not pt.save
+	end
+
+	test "Presence de nom" do
+		pt = Produit.create(ref: "a" * 7, prix: 0)
+		assert_not pt.save
+	end
+
+	test "Presence de prix" do
+		pt = Produit.create(ref: "a" * 7, nom: "name")
+		assert_not pt.save
+	end
+
+	test "Prix est un nombre" do
+		pt = Produit.create(ref: "a" * 7, nom: "name", prix:"p")
+		assert_not pt.save
+	end
+
+	test "Prix est un nombre >= 0" do
+		pt = Produit.create(ref: "a" * 7, nom: "name", prix: -1)
+		assert_not pt.save
+	end
+
+	test "Ref.lenght >= 6" do
+		pt = Produit.create(nom: "name", prix: 0, ref: "a"*5)
+		assert_not pt.save
+	end
+
+	test "Ref.lenght < 20" do
+		pt = Produit.create(nom: "name", prix: 0, ref: "a"*21)
+		assert_not pt.save
+	end
+
 end
