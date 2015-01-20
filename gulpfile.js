bf25b3556643d33b3b3a8eb2a15c16657e798a4b
MPur <dev.mpur@gmail.com>
2015-01-20 16:26:23 +0100

    Modification des fichiers de gridle pour compiler avec libsass

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,50 @@
 var gulp = require('gulp'),
-  livereload = require('gulp-livereload'),
-  plumber = require('gulp-plumber'),
-  uglify = require('gulp-uglify'),
-  concat = require('gulp-concat'),
-  newer = require('gulp-newer'),
-  changed = require('gulp-changed'),
-  cache = require('gulp-cached'),
-	sass = require('gulp-ruby-sass');
+  $ = require('gulp-load-plugins')(),
+  assetsDir = 'app/assets/',
+  src = {
+    javascripts: assetsDir + 'javascripts/',
+    stylesheets: assetsDir + 'stylesheets/'
+  },
+  dest = {
+    javascripts: 'public/javascripts',
+    css: 'public/stylesheets'
+  };
 
-
-var assetsDir = 'app/assets/';
-
-var src = {
-  javascripts: assetsDir + 'javascripts/',
-  stylesheets: assetsDir + 'stylesheets/'
-};
-
-var dest = {
-  javascripts: 'public/javascripts',
-  css: 'public/stylesheets'
-};
 
 gulp.task('scss', function(){
     return gulp.src(src.stylesheets + 'application.scss')
-        .pipe(changed(dest.css))
-        .pipe(cache('scss'))
-        .pipe(plumber())
-        .pipe(sass({
-        	sourcemap: false,
-          cacheLocation: assetsDir + '.sass-cache/'
+        .pipe($.sass({
+          onError: console.error.bind(console, 'SCSS error : ')
         }))
         .pipe(gulp.dest(dest.css));
 })
 
 
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src(src.javascripts + '**/*.js')
-        .pipe(cache('scripts'))
-        .pipe(plumber())
-        .pipe(concat('app.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(dest.javascripts));
-});
-
-gulp.task('watch', ['scss', 'scripts'], function(){
-  	var server = livereload();
-  	gulp.watch(src.stylesheets + '**/*', ['scss']).on('change', function(file) {
-  		console.log('le fichier ' + file.path + ' changé');
-    	server.changed(file.path);
-  	});
-
-    gulp.watch(src.javascripts + '**/*', ['scripts']).on('change', function(file) {
-      console.log('le fichier ' + file.path + ' changé');
-      server.changed(file.path);
-    });
+gulp.task('default', function(){
+       gulp.watch(src.stylesheets + '**/*', ['scss']);
 })
 
-gulp.task('default', ['scss', 'scripts']);
+// Concatenate & Minify JS
+// gulp.task('scripts', function() {
+//     return gulp.src(src.javascripts + '**/*.js')
+//         .pipe(cache('scripts'))
+//         .pipe(plumber())
+//         .pipe(concat('app.min.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest(dest.javascripts));
+// });
+
+// gulp.task('watch', ['scss', 'scripts'], function(){
+//   	var server = livereload();
+//   	gulp.watch(src.stylesheets + '**/*', ['scss']).on('change', function(file) {
+//   		console.log('le fichier ' + file.path + ' changé');
+//     	server.changed(file.path);
+//   	});
+
+//     gulp.watch(src.javascripts + '**/*', ['scripts']).on('change', function(file) {
+//       console.log('le fichier ' + file.path + ' changé');
+//       server.changed(file.path);
+//     });
+// })
+
+// gulp.task('default', ['scss', 'scripts']);
