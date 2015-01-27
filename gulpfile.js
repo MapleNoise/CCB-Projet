16198ca02178a55b90f1a8f1bf41847a0779ae24
MPur <dev.mpur@gmail.com>
2015-01-27 16:09:55 +0100

    Mise jour du gulpfile. Ajout des notifications et des compilation javascript

--- gulpfile.js
+++ gulpfile.js
@@ -17,19 +17,38 @@ gulp.task('browser-sync', function() {
         proxy: "localhost:3000",
         startPath: "/design.html",
         port: 8080,
+        open: false,
         files: ["app/views/**/*.html.erb", 'public/*.html']
     });
 });
 
 
 gulp.task('scss', function(){
+
+    var onError = function(err) {
+        $.notify.onError({
+            title:    "Gulp",
+            subtitle: "Failure!",
+            message:  "Errreur de compilation SCSS",
+            sound:    "Beep"
+        })(err);
+        console.log(err);
+    };
+
     return gulp.src(src.stylesheets + 'application.scss')
+        // .pipe($.plumber({errorHandler: onError }))
         .pipe($.sass({
-          onError: console.error.bind(console, 'SCSS error : ')
+          onError: onError
         }))
         .pipe(gulp.dest(dest.css))
         .pipe($.size())
-        .pipe(browserSync.reload({stream:true}));
+        .pipe(browserSync.reload({stream:true}))
+        .pipe($.notify({ // Add gulpif here
+           title: 'Gulp',
+           subtitle: 'success',
+           message: 'Fichiers SCSS compilé avec succes',
+           sound: "Pop"
+       }));
 })
 
 
@@ -37,18 +56,36 @@ gulp.task('default', ['browser-sync'], function(){
        // gulp.watch('app/views/**/*.html.erb', browserSync.reload());
        // gulp.watch('public/*.html', browserSync.reload());
        gulp.watch(src.stylesheets + '**/*', ['scss']);
+       gulp.watch(src.javascripts + '**/*', ['js']);
 })
 
 
 // Concatenate & Minify JS
-// gulp.task('scripts', function() {
-//     return gulp.src(src.javascripts + '**/*.js')
-//         .pipe(cache('scripts'))
-//         .pipe(plumber())
-//         .pipe(concat('app.min.js'))
-//         .pipe(uglify())
-//         .pipe(gulp.dest(dest.javascripts));
-// });
+gulp.task('js', function() {
+
+    var onError = function(err) {
+        $.notify.onError({
+            title:    "Gulp",
+            subtitle: "Failure!",
+            message:  "Errreur de compilation javascript",
+            sound:    "Beep"
+        })(err);
+        console.log(err);
+    };
+
+
+    return gulp.src([src.javascripts + 'rails.js', src.javascripts + '**/*.js'])
+        .pipe($.plumber({errorHandler: onError }))
+        .pipe($.concat('app.min.js'))
+        .pipe($.uglify())
+        .pipe(gulp.dest(dest.javascripts))
+        .pipe($.notify({ // Add gulpif here
+           title: 'Gulp',
+           subtitle: 'success',
+           message: 'Fichiers javascripts concaténés puis minifié',
+           sound: "Pop"
+       }));
+});
 
 // gulp.task('watch', ['scss', 'scripts'], function(){
 //   	var server = livereload();
