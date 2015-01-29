3fa02e30874de02cdbbc32653149160ba48acdc7
Idir BELAID <belaid.idir@gmail.com>
2015-01-29 13:52:00 +0100

    Commit categories filtre fonctionnelle + retouche d'image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,8 @@
 	$('.button-filter').on('click', function(e){
 		$(this).toggleClass('is-active');
 	})
+	$('.button-categorie').on('click', function(e){
+		$(this).toggleClass('is-active');
+	})
 
 })(jQuery)
