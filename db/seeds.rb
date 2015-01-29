2aaa140134a1234aa7f7a608c68b7dfd24185604
ThomasAy <thomas@phonations.com>
2015-01-29 18:34:40 +0100

    ajout des produits dans le seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,6 +37,9 @@ ExtensionFichier.create(extension: ".docm")#, type_produits_id: TypeProduit.find
 
 ExtensionFichier.create(extension: "youtube")
 
+ExtensionFichier.create(extension: ".jpg")
+
+
 TypeProduit.create(
   nom: "Fichier Excel",
   :extension_fichiers => ExtensionFichier.where(:extension => [".xls", ".csv"]))
@@ -44,6 +47,10 @@ TypeProduit.create(
 TypeProduit.create(
 	nom: "Fichier Word",
 	:extension_fichiers => ExtensionFichier.where(:extension => [".doc", ".docx", ".rtf", ".docm"]))
+
+TypeProduit.create(
+  nom: "Images",
+  :extension_fichiers => ExtensionFichier.where(:extension => [".jpg"]))
 
 TypeProduit.create(
   nom: "Vidéo",
@@ -205,46 +212,115 @@ form13 = Formation.create(
 form13.tags << Tag.find_by(:nom => "L'analyse")
 form13.tags << Tag.find_by(:nom => "Quizz", :mere => Tag.find_by(:nom => "L'analyse"))
 
+urlSender = FichierGeneralUploader.new
+urlSender.cache!(File.open(Dir.pwd + "/Bonsai_IMG_6404.jpg"))
+
 # Création de produits
-produit1 = Produit.create(
+produit1 = Produit.new(
   ref: "80ba14f781d79164",
   nom: "Produit 1",
   prix: 10.0,
-  type_produits_id: 1,
+  type_produits_id: 3,
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.",
   descriptionCourte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus.",
-  estPublic: false,
-  urlFichier: "Lorem_ipsum_dolor_sit_amet.docx")
+  estPublic: false)
   
+produit1.urlFichier = urlSender
 produit1.tags << Tag.find_by(:nom => "Le constat")
 produit1.tags << Tag.find_by(:nom => "Formation", :mere => Tag.find_by(:nom => "Le constat"))
+produit1.save
 
 
-produit2 = Produit.create(
+produit2 = Produit.new(
   ref: "90ba14f781d72264",
   nom: "Produit 2",
   prix: 10.0,
-  type_produits_id: 2,
+  type_produits_id: 3,
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.",
   descriptionCourte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus.",
-  estPublic: true,
-  urlFichier: "Lorem_ipsum_dolor_sit_amet.docx")
+  estPublic: true)
   
+produit2.urlFichier = urlSender
+
 produit2.tags << Tag.find_by(:nom => "L'analyse")
 produit2.tags << Tag.find_by(:nom => "Quizz", :mere => Tag.find_by(:nom => "L'analyse"))
+produit2.save
 
-produit3 = Produit.create(
+produit3 = Produit.new(
   ref: "90ba14f781d12464",
   nom: "Produit 3",
   prix: 10.0,
   type_produits_id: 3,
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.",
   descriptionCourte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus.",
-  estPublic: true,
-  urlFichier: "Lorem_ipsum_dolor_sit_amet.docx")
+  estPublic: true)
   
+produit3.urlFichier = urlSender
+
 produit3.tags << Tag.find_by(:nom => "L'analyse")
 produit3.tags << Tag.find_by(:nom => "Quizz", :mere => Tag.find_by(:nom => "L'analyse"))
+produit3.save
+
+produit4 = Produit.new(
+  ref: "90ba14f78aze2464",
+  nom: "Produit 4",
+  prix: 10.0,
+  type_produits_id: 3,
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.",
+  descriptionCourte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus.",
+  estPublic: true)
+  
+produit4.urlFichier = urlSender
+
+produit4.tags << Tag.find_by(:nom => "L'analyse")
+produit4.tags << Tag.find_by(:nom => "Quizz", :mere => Tag.find_by(:nom => "L'analyse"))
+produit4.save
+
+produit5 = Produit.new(
+  ref: "90ba14ze81d12464",
+  nom: "Produit 5",
+  prix: 10.0,
+  type_produits_id: 3,
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.",
+  descriptionCourte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus.",
+  estPublic: true)
+  
+produit5.urlFichier = urlSender
+
+produit5.tags << Tag.find_by(:nom => "L'analyse")
+produit5.tags << Tag.find_by(:nom => "Quizz", :mere => Tag.find_by(:nom => "L'analyse"))
+produit5.save
+
+produit6 = Produit.new(
+  ref: "90basdfd81d12464",
+  nom: "Produit 6",
+  prix: 10.0,
+  type_produits_id: 3,
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.",
+  descriptionCourte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus.",
+  estPublic: true)
+  
+produit5.urlFichier = urlSender
+
+produit5.tags << Tag.find_by(:nom => "L'analyse")
+produit5.tags << Tag.find_by(:nom => "Quizz", :mere => Tag.find_by(:nom => "L'analyse"))
+produit5.save
+
+produit6 = Produit.new(
+  ref: "90ba1ddfg1d12464",
+  nom: "Produit 6",
+  prix: 10.0,
+  type_produits_id: 3,
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.",
+  descriptionCourte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus.",
+  estPublic: true)
+  
+produit6.urlFichier = urlSender
+
+produit6.tags << Tag.find_by(:nom => "L'analyse")
+produit6.tags << Tag.find_by(:nom => "Quizz", :mere => Tag.find_by(:nom => "L'analyse"))
+produit6.save
+
 
 # Création des status
 Status.create([{nom: "En ligne"},
