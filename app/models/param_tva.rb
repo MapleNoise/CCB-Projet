0b8a927a7e26435f484dbd4e7351a7e3c1e92254
Shadaen <mangel.maxime@outlook.com>
2015-03-11 15:01:23 +0100

    Clean Up WhiteSpace

--- app/models/param_tva.rb
+++ app/models/param_tva.rb
@@ -1,6 +1,6 @@
 #encoding: UTF-8
 class ParamTva < ActiveRecord::Base
-  
+
   validates_presence_of :label, :valeur
   validates :valeur, numericality: { :greater_than_or_equal_to => 0 }
   def update(params)
@@ -8,15 +8,15 @@ class ParamTva < ActiveRecord::Base
     # Si on veut faire une modification il faut créer une nouvelle TVA
     fail "La TVA ne peut pas être modifiée"
   end
-  
+
   def getValideTVA(date=Time.now)
-    return ParamTva.where(["created_at < :dateNow and 
+    return ParamTva.where(["created_at < :dateNow and
     (dateSuppression > :dateNow or dateSuppression is null)",
         {dateNow: date}])
   end
-  
+
   def delete!
     update_attribute(:dateSuppression, Time.now)
   end
-  
+
 end
--- app/models/reponse.rb
+++ app/models/reponse.rb
@@ -1,11 +1,11 @@
 class Reponse < ActiveRecord::Base
-  
-  
+
+
   has_one :question
-  
+
   # Gestion de la suppression d'un produit
   def delete!
     update_attribute(:dateSuppression, Time.now)
   end
-  
+
 end
