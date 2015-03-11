b424b8a92195ea3cf2701e26e0b1531115a63ef2
Loïck M <loickmasson.mail@gmail.com>
2015-03-11 15:10:51 +0100

    fitlres stylisés + sort table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+	def sortable(column, title = nil)
+  	title ||= column.titleize
+  	if column == sort_column
+  		css_class = "sort sort__active sort--#{sort_direction}"
+  	else 
+  		css_class = "sort"
+  	end
+  	direction = 
+  	if column == sort_column && sort_direction == "asc"
+  		direction = "desc"
+  	else
+  		direction = "asc"
+  	end
+  		link_to title, {:sort => column, :direction => direction, :nom =>params[:nom], :prenom=>params[:prenom], :email=>params[:email]}, {:class => css_class}
+
+	end
+
 end
