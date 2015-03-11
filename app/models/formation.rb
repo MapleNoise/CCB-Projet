a587de6d08735998b9020e03c338e5832acff158
ThomasAy <thomas@phonations.com>
2015-03-11 16:03:15 +0100

    fix #48

--- app/models/formation.rb
+++ app/models/formation.rb
@@ -3,6 +3,7 @@ class Formation < ActiveRecord::Base
   validates_presence_of :nom
   validates :nom, length: { minimum: 4 , too_short: "Le minimum requis est de %{count} caracteres"}
   validates :nom, length: { maximum: 50 , too_long: "Le maximum requis est de %{count} caracteres"}
+  validates :prix, numericality: true
 
   has_many :sections
   has_and_belongs_to_many :tags
